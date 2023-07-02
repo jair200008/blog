d270946a598da9d86ed433fd489fc15e16e2bbb0
Jair Gaona <jairgaona60@gmail.com>
2023-07-02 14:17:42 -0500

    se un script para aprender el manejo

---
+++ script.js
@@ -0,0 +1,12 @@
+const elements = document.getElementById("section-Dos");
+const divOne = elements.querySelectorAll("div");
+console.log(elements.childNodes);
+
+divOne.forEach(function (element, index) {
+  console.log(element, index);
+  if (index % 2 !== 0) {
+    element.style.backgroundColor = "red";
+  } else {
+    element.style.backgroundColor = "blue";
+  }
+});
